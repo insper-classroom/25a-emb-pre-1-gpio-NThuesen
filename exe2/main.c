61de0732735ecf55ea64d71d623c829acbc508f0
NThuesen <nicholasnt@al.insper.edu.br>
2025-02-11 14:19:23 -0300

    exercicio 2

--- exe2/main.c
+++ exe2/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int BTN__PIN_AZUL = 7;
 
 int main() {
   stdio_init_all();
@@ -11,11 +12,20 @@ int main() {
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
 
+  gpio_init(BTN__PIN_AZUL);
+  gpio_set_dir(BTN__PIN_AZUL, GPIO_IN);
+  gpio_pull_up(BTN__PIN_AZUL);
+
   while (true) {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
       while (!gpio_get(BTN_PIN)) {
-      };
+      }
+    }
+    else if (!gpio_get(BTN__PIN_AZUL)){
+      printf("Botao 2\n");
+      while (!gpio_get(BTN__PIN_AZUL)){
+      }
     }
   }
 }
--- exe3/main.c
+++ exe3/main.c
@@ -2,8 +2,15 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_Vermelho = 28;
+const int BTN_Verde = 26;
+const int LED_Vermelho = 4;
+const int LED_Verde = 6;
+
 int main() {
   stdio_init_all();
+
+  gpio_init(BTN_Verde);
 
   while (true) {
   }
